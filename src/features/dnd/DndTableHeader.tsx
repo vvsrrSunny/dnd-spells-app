217e255c4e7cd936a9a2ca87eaa69546bf28962f
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-02-25 23:40:22 +0800

    updated the names

--- src/features/dnd/DndTableHeader.tsx
+++ src/features/dnd/DndTableHeader.tsx
@@ -2,13 +2,13 @@ import TableHeaderCell from "./TableHeaderCell";
 
 const DndTableHeader = () => {
     return (
-        <tr>
+        <>
             <TableHeaderCell>Index</TableHeaderCell>
             <TableHeaderCell>name</TableHeaderCell>
             <th scope="col" className="relative py-3 pl-3 pr-4 sm:pr-6">
                 <span className="sr-only">view</span>
             </th>
-        </tr>
+        </>
     );
 }
 
