c8ca85bb9fdfa2ff92a8555f4ff0761e8e2acdee
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-02-25 17:00:48 +0800

    async is not needed in this use case

--- src/features/dnd/dndSlice.ts
+++ src/features/dnd/dndSlice.ts
@@ -1,5 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface DndResult {
     index: string;
@@ -7,27 +6,16 @@ export interface DndResult {
     url: string;
 }
 
-// Define the async thunk
-export const fetchData = createAsyncThunk('data/fetchData', async () => {
-    const response = await axios.get('http://www.dnd5eapi.co/api/spells');
-    return response.data;
-});
-
 export interface DndState {
     count: number;
     result: Array<DndResult>;
-    status?: string,
-    error?: string | null,
 }
 
 const initialState: DndState = {
     count: 0,
     result: [
     ],
-    status: 'idle',
-    error: null,
 };
-
 
 export const dndSlice = createSlice({
     name: 'dnd',
@@ -40,24 +28,6 @@ export const dndSlice = createSlice({
             state.result = action.payload.result;
         },
     },
-
-    extraReducers: (builder) => {
-        builder
-          .addCase(fetchData.pending, (state) => {
-            state.status = 'loading';
-          })
-          .addCase(fetchData.fulfilled, (state, action) => {
-            state.status = 'succeeded';
-            state.count = action.payload.count;
-            state.result = action.payload.result;
-          })
-          .addCase(fetchData.rejected, (state, action) => {
-            state.status = 'failed';
-            state.count = 0;
-            state.result = [];
-            state.error = 'failed to get dnd results';
-          });
-      },
 });
 
 export const { setState } = dndSlice.actions;
