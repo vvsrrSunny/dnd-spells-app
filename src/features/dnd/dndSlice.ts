952552e95a7fabe8a577d6a75063eac46a34c93d
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-02-25 16:59:39 +0800

    example of setting when dispatch  action will make a aync call and update the state #2

--- src/features/dnd/dndSlice.ts
+++ src/features/dnd/dndSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export interface DndResult {
     index: string;
@@ -6,16 +7,27 @@ export interface DndResult {
     url: string;
 }
 
+// Define the async thunk
+export const fetchData = createAsyncThunk('data/fetchData', async () => {
+    const response = await axios.get('http://www.dnd5eapi.co/api/spells');
+    return response.data;
+});
+
 export interface DndState {
     count: number;
     result: Array<DndResult>;
+    status?: string,
+    error?: string | null,
 }
 
 const initialState: DndState = {
     count: 0,
     result: [
     ],
+    status: 'idle',
+    error: null,
 };
+
 
 export const dndSlice = createSlice({
     name: 'dnd',
@@ -28,6 +40,24 @@ export const dndSlice = createSlice({
             state.result = action.payload.result;
         },
     },
+
+    extraReducers: (builder) => {
+        builder
+          .addCase(fetchData.pending, (state) => {
+            state.status = 'loading';
+          })
+          .addCase(fetchData.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.count = action.payload.count;
+            state.result = action.payload.result;
+          })
+          .addCase(fetchData.rejected, (state, action) => {
+            state.status = 'failed';
+            state.count = 0;
+            state.result = [];
+            state.error = 'failed to get dnd results';
+          });
+      },
 });
 
 export const { setState } = dndSlice.actions;
