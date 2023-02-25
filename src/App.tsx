41cd684380dd2881e07a880a66bb3c7d6cd0703b
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-02-25 21:20:20 +0800

    added code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import AppLayout from "./layouts/AppLayout";
+import Dnd from "./features/dnd/Dnd";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <AppLayout>
-        <div>Hii this is the table will sit</div>
+      <Dnd></Dnd>
       </AppLayout>
     </div>
   );
--- src/features/dnd/Dnd.tsx
+++ src/features/dnd/Dnd.tsx
@@ -1,1 +1,7 @@
-export {}
+import React from "react";
+
+const Dnd = () => {
+    return (<div>this is dnd table</div>);
+};
+
+export default Dnd;
