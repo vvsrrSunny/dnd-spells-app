06a4ba4aff2c45563d92d756ac97a5b71cd353f9
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-02-26 23:50:31 +0800

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import AppLayout from "./components/AppLayout";
 import Dnd from "./features/dnd/Dnd";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import DndSpell from "./features/dndspell/DndSpell";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <div className="app">
       <AppLayout>
-      <Dnd></Dnd>
-      </AppLayout>
-    </div>
+        <Routes>
+          <Route path="/" element={<Dnd />}></Route>
+          <Route path="/api/spells/:index" element={<DndSpell/>}> </Route>
+          <Route path="*" element={<PageNotFound/>}> </Route>
+        </Routes>
+      </AppLayout >
+    </div >
   );
 }
 
