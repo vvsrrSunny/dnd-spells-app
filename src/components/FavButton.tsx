d1cdb82092fedc7247a64da600a3b6003f53963e
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-03-05 15:06:38 +0800

    made a fav button for abstraction

---
+++ src/components/FavButton.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import ShowHeartIcon from "./ShowHeartIcon";
+
+interface Props {
+    children?: ReactNode,
+    isFavourite: Boolean,
+    onFavouriteButtonClick: () => void,
+}
+
+const FavButton = ({ children, ...props }: Props) => {
+
+    return (<>
+        <button onClick={props.onFavouriteButtonClick} className="flex flex-row space-x-1 items-center bg-transparent hover:bg-indigo-500 text-indigo-700 font-thin text-sm hover:text-white py-2 px-2 border border-indigo-500 hover:border-transparent rounded">
+            <ShowHeartIcon color={`${props.isFavourite ? 'text-gray-400' : 'text-red-500'}`} />
+            <span> {`${props.isFavourite ? 'Remove from favourites' : 'Add to favourites'}`}</span>
+        </button>
+
+    </>
+    );
+}
+
+export default FavButton;
--- src/components/ShowDdnSpellFavourites.tsx
+++ src/components/ShowDdnSpellFavourites.tsx
@@ -1,6 +1,6 @@
 
-import { HeartIcon } from "@heroicons/react/24/outline";
 import { ReactNode, useState } from "react";
+import FavButton from "./FavButton";
 import ShowHeartIcon from "./ShowHeartIcon";
 import ThePrompt from "./ThePrompt";
 
@@ -15,16 +15,13 @@ const ShowDdnSpellFavourites = ({ children, ...props }: Props) => {
 
     const [promptOpen, setPromptOpen] = useState(false);
 
-    const OnFavouriteButtonClick = () => {
+    const onFavouriteButtonClick = () => {
         setPromptOpen(true);
     }
     return (<>
         <div className="flex justify-between items-center pb-6">
-            {props.isFavourite && (<ShowHeartIcon height="sm:h-8" width="sm:w-8" classNames="w-7 h-7"/>)}
-            <button onClick={OnFavouriteButtonClick} className="flex flex-row space-x-1 items-center bg-transparent hover:bg-indigo-500 text-indigo-700 font-thin text-sm hover:text-white py-2 px-2 border border-indigo-500 hover:border-transparent rounded">
-                <ShowHeartIcon color={`${props.isFavourite ? 'text-gray-400' : 'text-red-500'}`}/>
-                <span> {`${props.isFavourite ? 'Remove from favourites' : 'Add to favourites'}`}</span>
-            </button>
+            {props.isFavourite && (<ShowHeartIcon height="sm:h-8" width="sm:w-8" classNames="w-7 h-7" />)}
+            <FavButton onFavouriteButtonClick={onFavouriteButtonClick} isFavourite={props.isFavourite} />
         </div>
 
         <ThePrompt promptOpen={promptOpen} promptOpenedCallback={() => setPromptOpen(false)} onClickAccept={props.onClickFavouriteButton}></ThePrompt>
