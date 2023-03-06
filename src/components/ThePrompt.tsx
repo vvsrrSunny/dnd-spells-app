285cffedd5aa8f2fd814bf8cc0eec8f496007a19
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-03-06 15:18:17 +0800

    updated the propmt position

--- src/components/ThePrompt.tsx
+++ src/components/ThePrompt.tsx
@@ -51,7 +51,7 @@ const ThePrompt = (props: Props) => {
                 </Transition.Child>
 
                 <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div className="flex min-h-full items-start justify-center p-4 text-center sm:items-center sm:p-0">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
